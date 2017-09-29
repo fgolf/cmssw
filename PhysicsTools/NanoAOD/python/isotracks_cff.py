3da428de2aac48dfcca0f3fffb1bd0e91ed3d73f
Frank Golf (fgolf) <frank.golf@gmail.com>
2017-09-29 11:20:23 +0200

    first implementation of isolated tracks for nanoAOD

--- PhysicsTools/NanoAOD/python/isotracks_cff.py
+++ PhysicsTools/NanoAOD/python/isotracks_cff.py
@@ -3,11 +3,10 @@ from PhysicsTools.NanoAOD.common_cff import *
 
 finalIsolatedTracks = cms.EDProducer("IsolatedTrackCleaner",
     tracks = cms.InputTag("isolatedTracks"),
-    cut = cms.string("pt > 10 && abs(dxy) < 0.02 && abs(dz) < 0.1 && isHighPurityTrack && miniPFIsolation.chargedHadronIso/pt < 0.2"), 
+    cut = cms.string("((pt>5 && (abs(pdgId) == 11 || abs(pdgId) == 13)) || pt > 10) && (abs(pdgId) < 15 || abs(eta) < 2.5) && abs(dxy) < 0.2 && abs(dz) < 0.1 && isHighPurityTrack && ((pfIsolationDR03().chargedHadronIso < 5 && pt < 25) || pfIsolationDR03().chargedHadronIso/pt < 0.2)"), 
     finalLeptons = cms.VInputTag(
         cms.InputTag("finalElectrons"),
         cms.InputTag("finalMuons"),
-        cms.InputTag("finalTaus"),
     ),
 )
 
@@ -27,6 +26,8 @@ isoTrackTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     variables = cms.PSet(P3Vars,
         dz = Var("dz",float,doc="dz (with sign) wrt first PV, in cm",precision=10),
         dxy = Var("dxy",float,doc="dxy (with sign) wrt first PV, in cm",precision=10),
+	isPFcand = Var("?packedCandRef().isNonnull()?1:0",int,doc="if isolated track is a PF candidate"),
+	pdgId = Var("pdgId",int,doc="PDG id of PF cand"),
         PFIso03_chg = Var("pfIsolationDR03().chargedHadronIso",float,doc="PF isolation dR=0.3, charged component",precision=10),
         PFIso03_all = Var("(pfIsolationDR03().chargedHadronIso + max(pfIsolationDR03().neutralHadronIso + pfIsolationDR03().photonIso - pfIsolationDR03().puChargedHadronIso/2,0.0))",float,doc="PF isolation dR=0.3, total (deltaBeta corrections)",precision=10),
     ),
@@ -34,7 +35,7 @@ isoTrackTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
         miniPFIso_chg = ExtVar("isoForIsoTk:miniIsoChg",float,doc="mini PF isolation, charged component",precision=10),
         miniPFIso_all = ExtVar("isoForIsoTk:miniIsoAll",float,doc="mini PF isolation, total (with scaled rho*EA PU corrections)",precision=10),
     ),
-)
+)#
 
 isoTrackSequence = cms.Sequence(finalIsolatedTracks + isoForIsoTk)
 isoTrackTables = cms.Sequence(isoTrackTable)
--- PhysicsTools/NanoAOD/python/muons_cff.py
+++ PhysicsTools/NanoAOD/python/muons_cff.py
@@ -77,6 +77,7 @@ muonTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
         PFIso03_chg = Var("pfIsolationR03().sumChargedHadronPt",float,doc="PF isolation dR=0.3, charged component"),
         PFIso03_all = Var("(pfIsolationR03().sumChargedHadronPt + max(pfIsolationR03().sumNeutralHadronEt + pfIsolationR03().sumPhotonEt - pfIsolationR03().sumPUPt/2,0.0))",float,doc="PF isolation dR=0.3, total (deltaBeta corrections)"),
         tightCharge = Var("?(muonBestTrack().ptError()/muonBestTrack().pt() < 0.2)?2:0",int,doc="Tight charge criterion using pterr/pt of muonBestTrack (0:fail, 2:pass)"),
+	isPFcand = Var("?isPFMuon?1:0",int,doc="muon is PF candidate"),
     ),
     externalVariables = cms.PSet(
         mvaTTH = ExtVar(cms.InputTag("muonMVATTH"),float, doc="TTH MVA lepton ID score",precision=14),
