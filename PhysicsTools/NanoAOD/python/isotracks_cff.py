a792272279924160c9ee901211e0ce409b8d97cf
Frank Golf (fgolf) <frank.golf@gmail.com>
2017-09-29 16:22:26 +0200

    remove high purity requirement

--- PhysicsTools/NanoAOD/python/isotracks_cff.py
+++ PhysicsTools/NanoAOD/python/isotracks_cff.py
@@ -3,7 +3,7 @@ from PhysicsTools.NanoAOD.common_cff import *
 
 finalIsolatedTracks = cms.EDProducer("IsolatedTrackCleaner",
     tracks = cms.InputTag("isolatedTracks"),
-    cut = cms.string("((pt>5 && (abs(pdgId) == 11 || abs(pdgId) == 13)) || pt > 10) && (abs(pdgId) < 15 || abs(eta) < 2.5) && abs(dxy) < 0.2 && abs(dz) < 0.1 && isHighPurityTrack && ((pfIsolationDR03().chargedHadronIso < 5 && pt < 25) || pfIsolationDR03().chargedHadronIso/pt < 0.2)"), 
+    cut = cms.string("((pt>5 && (abs(pdgId) == 11 || abs(pdgId) == 13)) || pt > 10) && (abs(pdgId) < 15 || abs(eta) < 2.5) && abs(dxy) < 0.2 && abs(dz) < 0.1 && ((pfIsolationDR03().chargedHadronIso < 5 && pt < 25) || pfIsolationDR03().chargedHadronIso/pt < 0.2)"), 
     finalLeptons = cms.VInputTag(
         cms.InputTag("finalElectrons"),
         cms.InputTag("finalMuons"),
@@ -28,6 +28,7 @@ isoTrackTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
         dxy = Var("dxy",float,doc="dxy (with sign) wrt first PV, in cm",precision=10),
 	isPFcand = Var("?packedCandRef().isNonnull()?1:0",int,doc="if isolated track is a PF candidate"),
 	pdgId = Var("pdgId",int,doc="PDG id of PF cand"),
+	isHighPurityTrack = Var("isHighPurityTrack",int,doc="track is high purity"),
         PFIso03_chg = Var("pfIsolationDR03().chargedHadronIso",float,doc="PF isolation dR=0.3, charged component",precision=10),
         PFIso03_all = Var("(pfIsolationDR03().chargedHadronIso + max(pfIsolationDR03().neutralHadronIso + pfIsolationDR03().photonIso - pfIsolationDR03().puChargedHadronIso/2,0.0))",float,doc="PF isolation dR=0.3, total (deltaBeta corrections)",precision=10),
     ),
@@ -35,7 +36,7 @@ isoTrackTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
         miniPFIso_chg = ExtVar("isoForIsoTk:miniIsoChg",float,doc="mini PF isolation, charged component",precision=10),
         miniPFIso_all = ExtVar("isoForIsoTk:miniIsoAll",float,doc="mini PF isolation, total (with scaled rho*EA PU corrections)",precision=10),
     ),
-)#
+)
 
 isoTrackSequence = cms.Sequence(finalIsolatedTracks + isoForIsoTk)
 isoTrackTables = cms.Sequence(isoTrackTable)
